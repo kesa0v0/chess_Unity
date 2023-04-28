d01f1341044327b8724856bbe53f4683a5b90025
kesa <kesa0v0@gmail.com>
2023-04-29 01:22:42 +0900

    Added Knight

--- Assets/Scripts/Pieces/Knight.cs
+++ Assets/Scripts/Pieces/Knight.cs
@@ -1,10 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+
 public class Knight : Piece
 {
     public override MovableTiles GetMovableTilesCode()
     {
         var movableTiles = new MovableTiles();
         
-        movableTiles.Add(HorizontalMovement());
+        var pos = Board.GetPosFromVec2(transform.position);
+        var knightMove = new List<int>
+        {
+            pos + 21, pos + 19, pos + 12, pos + 8, pos - 21, pos - 19, pos - 12, pos - 8
+        };
+        
+        foreach (var move in knightMove.Where(move => !Board.GetPiece(move) || Board.GetPiece(move).Team != Team))
+        {
+            if (Board.GetPiece(move) && Board.GetPiece(move).Team != Team)
+            {
+                movableTiles.AddKillable(move);
+                continue;
+            }
+            movableTiles.AddMovable(move);
+        }
 
         return movableTiles;
     }
