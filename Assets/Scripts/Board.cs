bf4552efc4d0432e360adf1944f93af728a8657c
kesa <kesa0v0@gmail.com>
2023-05-26 18:43:25 +0900

    add check indicator

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,45 +2,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-[System.Serializable]
-public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
-{
-    [SerializeField]
-    private List<TKey> keys = new List<TKey>();
-
-    [SerializeField]
-    private List<TValue> values = new List<TValue>();
-
-    // save the dictionary to lists
-    public void OnBeforeSerialize()
-    {
-        keys.Clear();
-        values.Clear();
-        foreach (KeyValuePair<TKey, TValue> pair in this)
-        {
-            keys.Add(pair.Key);
-            values.Add(pair.Value);
-        }
-    }
-
-    // load dictionary from lists
-    public void OnAfterDeserialize()
-    {
-        this.Clear();
-
-        if (keys.Count != values.Count)
-            throw new System.Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable."));
-
-        for (int i = 0; i < keys.Count; i++)
-            this.Add(keys[i], values[i]);
-    }
-}
-
 public class Board : MonoBehaviour
 {
     [Header("BoardSettings")] 
     public bool isBoardFlipped;
 
+    [SerializeField] private SpriteRenderer whiteCheckIndicator;
+    [SerializeField] private SpriteRenderer blackCheckIndicator;
     [SerializeField] private SpriteRenderer turnIndicatorSpriteRenderer;
     [SerializeField] private SpriteRenderer boardSpriteRenderer;
     [SerializeField] private Color boardColor;
@@ -57,8 +25,8 @@ public class Board : MonoBehaviour
     private readonly List<Piece> _whitePieces = new();
     private readonly List<Piece> _blackPieces = new();
     
-    public SerializableDictionary<int, Pawn> EnPassantPawns = new();
-    public SerializableDictionary<int, Pawn> TempEnPassantPawns = new();
+    public readonly Dictionary<int, Pawn> EnPassantPawns = new();
+    public readonly Dictionary<int, Pawn> TempEnPassantPawns = new();
 
     // Start is called before the first frame update
     private void Start()
